59f8affd99f105dd32011f2441b8b2b37eee1b26
jeonggiju <halogiju123@gmail.com>
2025-10-28 18:32:45 +0900

    test(S3Storage): add unit tests for S3-based binary storage operations
    - Implement `S3BinaryStorageTest` to verify S3 upload and download functionality.
    - Add `aws.s3` configuration in `application-test.yml` for testing.

--- src/main/java/com/monew/monew_batch/storage/S3BinaryStorage.java
+++ src/main/java/com/monew/monew_batch/storage/S3BinaryStorage.java
@@ -1,9 +1,9 @@
 package com.monew.monew_batch.storage;
 
+import static com.monew.monew_batch.util.DataTimeParser.*;
+
 import java.io.InputStream;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
 @Slf4j
 @Component
@@ -28,7 +30,7 @@ public class S3BinaryStorage implements BinaryStorage {
 
 	@Override
 	public UUID put(UUID id, Instant date, byte[] data) {
-		String formattedDate = getDate(date);
+		String formattedDate = getDateFromInstant(date);
 		String key = PATH + formattedDate + "/" + id;
 
 		s3.putObject(b -> b.bucket(config.getBucket())
@@ -39,16 +41,31 @@ public class S3BinaryStorage implements BinaryStorage {
 
 	@Override
 	public InputStream get(UUID id, Instant date) {
-		String formattedDate = getDate(date);
+		String formattedDate = getDateFromInstant(date);
 		String key = PATH + formattedDate + "/" + id;
 
 		return s3.getObject(b -> b.bucket(config.getBucket())
 			.key(key));
 	}
 
-	private String getDate(Instant date) {
-		return DateTimeFormatter.ofPattern("yyyy-MM-dd")
-			.withZone(ZoneId.of("Asia/Seoul"))
-			.format(date);
+	@Override
+	public Boolean exists(UUID id, Instant date) {
+		String formattedDate = getDateFromInstant(date);
+		String key = PATH + formattedDate + "/" + id;
+
+		try {
+			HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+				.bucket(config.getBucket())
+				.key(key)
+				.build();
+
+			s3.headObject(headObjectRequest);
+			return true;
+		} catch (NoSuchKeyException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+
 	}
 }
