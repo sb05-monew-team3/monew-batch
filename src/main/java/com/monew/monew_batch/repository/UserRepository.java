70cfb50c7ba38a96f6071fd7cf0915431c74e2bd
jeonggiju <halogiju123@gmail.com>
2025-11-04 11:29:52 +0900

    chore(UserRepository): deleteByDeletedAtBefore 추가

--- src/main/java/com/monew/monew_batch/repository/UserRepository.java
+++ src/main/java/com/monew/monew_batch/repository/UserRepository.java
@@ -1,16 +1,22 @@
 package com.monew.monew_batch.repository;
 
-import com.monew.monew_batch.entity.User;
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
+import com.monew.monew_batch.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-	long deleteUsersMarkedAsDeleted();
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM User u WHERE u.deletedAt < :threshold")
+	int deleteByDeletedAtBefore(@Param("threshold") Instant threshold);
 }
