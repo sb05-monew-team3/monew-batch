9946a447a3d3e5cb6ba51a1698c3db042a208400
jeonggiju <halogiju123@gmail.com>
2025-11-04 11:10:36 +0900

    chore: 불필요한 @Transaction 제거

--- src/main/java/com/monew/monew_batch/schedule/UserCleanupScheduler.java
+++ src/main/java/com/monew/monew_batch/schedule/UserCleanupScheduler.java
@@ -1,11 +1,11 @@
 package com.monew.monew_batch.schedule;
 
+import org.springframework.stereotype.Component;
+
 import com.monew.monew_batch.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
@@ -14,13 +14,12 @@ public class UserCleanupScheduler {
 
 	private final UserRepository userRepository;
 
-	@Transactional
-	@Scheduled(cron = "0 0 0 * * *")
+	// @Scheduled(cron = "0 0 0 * * *")
 	public void cleanupDeletedUsers() {
 		log.info("[UserCleanupScheduler] 삭제 스케줄러 실행 시작");
 
 		try {
-			long count = userRepository.deleteUsersMarkedAsDeleted();
+			long count = userRepository.deleteByDeletedAtIsNotNull();
 			log.info("[UserCleanupScheduler] 삭제된 사용자 수: {}", count);
 		} catch (Exception e) {
 			log.error("[UserCleanupScheduler] 삭제 중 오류 발생: {}", e.getMessage(), e);
