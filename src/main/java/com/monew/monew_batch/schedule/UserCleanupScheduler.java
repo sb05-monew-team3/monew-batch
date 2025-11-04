09804fd14e04435ec94aff2e0f138221e2bd0f1d
jeonggiju <halogiju123@gmail.com>
2025-11-04 11:30:24 +0900

    chore(UserCleanupSchedeler): UserRepository.deleteByDeletedAtBefore 추가에 따른 메서드 수정

--- src/main/java/com/monew/monew_batch/schedule/UserCleanupScheduler.java
+++ src/main/java/com/monew/monew_batch/schedule/UserCleanupScheduler.java
@@ -1,5 +1,9 @@
 package com.monew.monew_batch.schedule;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.monew.monew_batch.repository.UserRepository;
@@ -14,13 +18,14 @@ public class UserCleanupScheduler {
 
 	private final UserRepository userRepository;
 
-	// @Scheduled(cron = "0 0 0 * * *")
+	@Scheduled(cron = "0 0 1 * * *")
 	public void cleanupDeletedUsers() {
 		log.info("[UserCleanupScheduler] 삭제 스케줄러 실행 시작");
 
 		try {
-			long count = userRepository.deleteByDeletedAtIsNotNull();
-			log.info("[UserCleanupScheduler] 삭제된 사용자 수: {}", count);
+			Instant threshold = Instant.now().minus(1, ChronoUnit.DAYS);
+			int i = userRepository.deleteByDeletedAtBefore(threshold);
+			log.info("[UserCleanupScheduler] 삭제 완료 : {}", i);
 		} catch (Exception e) {
 			log.error("[UserCleanupScheduler] 삭제 중 오류 발생: {}", e.getMessage(), e);
 		}
